c9b36d0dca6748a81044c90ffe39b6b276af464e
Sungwoo-Lee <sungwoo320@gmail.com>
2026-02-21 17:27:35 +0900

    docs: expand DreamerV3 comparison with detailed architectural, functional, and training loop analysis.

--- train_command.sh
+++ train_command.sh
@@ -7,9 +7,9 @@ set -e
 # neuromodulated_dreamer_v3
 
 python train.py \
-  --agent_config configs/models/neuromodulated_ppo.yaml \
+  --agent_config configs/models/dreamer_v3.yaml \
   --num-envs 128 \
   --episodes 10000000 \
   --checkpoint-frequency 1000000  \
   --device cuda:1 \
-  --tag "rppoNMN_128env_Multip_G40"
+  --tag "dreamer_v3_128env"
