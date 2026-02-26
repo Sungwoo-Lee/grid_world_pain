1294c30d2b4db00de5d60c6668bde40bcfd8bccc
Sungwoo-Lee <sungwoo320@gmail.com>
2026-02-26 13:58:32 +0900

    feat: Implement DreamerV3 replay ratio and collect interval to dynamically control gradient steps, replacing the fixed train_steps parameter.

--- train_command.sh
+++ train_command.sh
@@ -8,8 +8,8 @@
 /home/vncuser/miniconda3/envs/grid_world_pain/bin/python train.py \
   --config configs/experiment/ablation/homeostatic/08_location.yaml \
   --agent_config configs/models/dreamer_v3.yaml \
-  --num-envs 64 \
-  --episodes 100000 \
+  --num-envs 1 \
+  --episodes 1000000 \
   --checkpoint-frequency 10000 \
-  --device cuda:1 \
-  --tag "08_location_dreamer_v3_64env_64trainStep_fixedTwoHot"
+  --device cuda:0 \
+  --tag "08_location_dreamer_v3_1env_1trainStep_fixedTwoHot_1M"
